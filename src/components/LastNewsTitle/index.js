13dc8e48d80f32c52ba3f18f256f35ffb9e2f564
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-01-11 14:25:18 -0300

    LastNewTitle criado

---
+++ src/components/LastNewsTitle/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const LastNewsTitle = () => {
+    return (
+        <div className="lastNewsTitle">
+            <h1>Últimas Notícias</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default LastNewsTitle;
