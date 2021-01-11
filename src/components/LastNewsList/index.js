bedc10ebcd7ecf384f6ca791dad6a94ed5048933
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-01-11 14:12:20 -0300

    Grid das últimas notícias criado

---
+++ src/components/LastNewsList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LastNews from '../LastNews';
+import './index.css';
+
+const LastNewsList = () => {
+    return (
+        <div className="lastNewsList">
+            <LastNews/>
+        </div>     
+    );
+}
+
+export default LastNewsList;
