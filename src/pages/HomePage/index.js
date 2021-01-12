6b8f44c7cd99ce1736a58fd0570ad5a01d2f35cd
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-01-12 14:45:10 -0300

    Adicionada classe na tag main

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -3,12 +3,13 @@ import Header from '../../components/Header';
 import LastNewsList from '../../components/LastNewsList';
 import LastNewsTitle from '../../components/LastNewsTitle';
 import Topic from '../../components/Topic';
+import './index.css';
 
 const HomePage = () => {
     return (
         <>
-            <Header/>
-            <main>
+            <Header type="main"/>
+            <main className="homeContent">
                 <Topic type="Processo Seletivo"/>
                 <Topic type="Feira Regional"/>
                 <Topic type="Alunos Prêmios"/>
