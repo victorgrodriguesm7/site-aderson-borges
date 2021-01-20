96fdef66b189127804d8b32377af868189d693ab
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-01-20 12:19:52 -0300

    API adicionada

--- src/components/LastNews/index.js
+++ src/components/LastNews/index.js
@@ -1,42 +1,58 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import app from '../../services/firebase';
 import './index.css';
 
 const LastNews = () => {
+    const history = useHistory();
     let [lastNews, setLastNews] = useState(null);
     let [loading, setLoading] = useState(true);
-    function getLoadingComponents(){
-        let loading = [];
-        for (var i = 0; i <= 3; i++){
-            loading.push( 
+
+    function handleRedirect(id) {
+        history.push(`/noticia?article=${id}`);
+    }
+    function getPlaceHolderComponents() {
+        let PlaceHolders = [];
+        for (var i = 0; i <= 3; i++) {
+            PlaceHolders.push(
                 <div className="loadingNews" key={i}>
-                    <div className="loadingImage"/>
+                    <div className="loadingImage" />
                     <div className="loadingContent">
-                        <div className="loadingTitle"/>
+                        <div className="loadingTitle" />
                         <div className="loadingDescription">
-                            <hr/>
-                            <hr/>
-                            <hr/>
+                            <hr />
+                            <hr />
+                            <hr />
                         </div>
                     </div>
                 </div>
             );
         }
-        return loading;
+        return PlaceHolders;
     }
-    async function getLastNews(){
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve([
-                    {
-                        title: "Ceará Comemora 10 anos de EEEP's",
-                        thumb: "https://www.seduc.ce.gov.br/wp-content/uploads/sites/37/2019/09/Foto-EEEP-1200x675.png",
-                        description: "A qualificação profissional é requisito indispensável para o acesso ao universo do trabalho. Mais do que isso, pode representar a realização pessoal e a conquista da própria dignidade."
-                    }
-                ])
-            }, [1000])
-        });
+
+    async function getLastNews() {
+        let docs = (await app.firestore().collection("articles").orderBy("date", "desc").get()).docs
+        let news = [];
+
+        for (let doc of docs){
+            let id = doc.id;
+            let { title, description, topic, thumbExtension } = doc.data()
+            let thumb =  await app.storage().ref(`images/${id}`).child(`thumb${thumbExtension}`)
+                .getDownloadURL();
+            news.push({
+                title,
+                description,
+                thumb,
+                id
+            });
+        }
+
+        return news;
     }
+
     useEffect(() => {
+        
         getLastNews().then(value => {
             setLastNews(value);
             setLoading(false);
@@ -44,12 +60,12 @@ const LastNews = () => {
     }, [])
 
     if (loading)
-        return getLoadingComponents();
-    
+        return getPlaceHolderComponents();
+
     return lastNews.map(news => {
         return (
-            <div className="newsContainer">
-                <img className="newsImage" src={news.thumb} alt={news.title}/>
+            <div className="newsContainer" key={news.id} onClick={e => handleRedirect(news.id)}>
+                <img className="newsImage" src={news.thumb} alt={news.title} />
                 <div className="newsContent">
                     <h3 className="newsTitle">{news.title}</h3>
                     <div className="newsDescription">{news.description}</div>
