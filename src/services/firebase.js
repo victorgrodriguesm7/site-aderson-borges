93703c70ce313c0cf653f3594e2f8eaeb1d0e188
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-01-20 12:22:23 -0300

    Adicionado Firestore e Firestorage

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
-
+import 'firebase/firestore';
+import 'firebase/storage'
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
