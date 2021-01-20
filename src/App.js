c9ee6e6b59b712733c92d3f556e7fb5c65d76152
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-01-20 12:18:58 -0300

    Adicionado path para notícias

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import './globals.css';
+import NewsPage from './pages/NewsPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/noticia" component={NewsPage}/>
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/sobre" component={AboutPage}/>
       </Switch>
